8eea4d4df7817b0bea72d5acb80b7e4816b2bb8a
S-Rad <shima.radfar@txs.de>
2020-10-27 16:17:59 +0100

    background displayed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import { Box } from '@material-ui/core'
 import background from './assets/background-main.jpg'
 
 function App() {
-    console.log(background)
     return (
-        <Box display="flex" flexDirection="column" alignItems="center" style={{ backgroundImage: 'url()' }}>
+        <Box display="flex" flexDirection="column" alignItems="center" style={{ backgroundImage: `url(${background})` }}>
             <Box display="flex" width="80vw" flexWrap="wrap">
                 {[...Array(24).keys()].map(day => (
                     <Door day={day + 1} />
