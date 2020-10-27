7679fb908aa876d38d8587cf24bc8edecd472461
S-Rad <shima.radfar@txs.de>
2020-10-27 12:02:17 +0100

    boxes wrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Box } from '@material-ui/core'
 function App() {
     return (
         <Box display="flex" flexDirection="column" alignItems="center">
-            <Box display="flex" width="80vw" flexDirection="column" alignItems="center">
+            <Box display="flex" width="80vw" flexWrap="wrap">
                 {[...Array(24).keys()].map(day => (
                     <Door day={day + 1} />
                 ))}
