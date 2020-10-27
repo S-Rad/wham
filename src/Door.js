2964d1ab0ef5f1f6473bd082a305b16b31bcb180
S-Rad <shima.radfar@txs.de>
2020-10-27 12:40:52 +0100

    Dialog init

--- src/Door.js
+++ src/Door.js
@@ -1,11 +1,21 @@
-import { Box } from '@material-ui/core'
+import { Box, Dialog } from '@material-ui/core'
+import { useState } from 'react'
 
-const Door = ({ day, imagepath }) => (
-    <Box height="100px" width="100px" display="flex" flexDirection="column" alignItems="center">
-        <Box display="flex" height="100%" alignItems="center">
-            {day}
-        </Box>
-    </Box>
-)
+const Door = ({ day, imagepath }) => {
+    const [open, setOpen] = useState(false)
+    console.log(open)
+    return (
+        <>
+            <Box height="100px" width="100px" display="flex" flexDirection="column" alignItems="center" onClick={e => setOpen(true)}>
+                <Box display="flex" height="100%" alignItems="center">
+                    {day}
+                </Box>
+            </Box>
+            <Dialog open={open} onClose={e => setOpen(false)} onClick={e => setOpen(false)}>
+                Hello World
+            </Dialog>
+        </>
+    )
+}
 
 export default Door
