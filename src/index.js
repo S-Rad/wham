a03717a8d7232b443c49002bed01f64af32ee68d
Felix Tietjen <felix@tietjen.it>
2020-10-28 16:06:25 +0100

    fix warn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { CssBaseline, makeStyles } from '@material-ui/core'
+import { CssBaseline } from '@material-ui/core'
 
 import 'fontsource-mountains-of-christmas'
-
-
 
 ReactDOM.render(
     <>
