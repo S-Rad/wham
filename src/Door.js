f6c9c6a5c57cb996ef2ef4a2f52419f15cb5ad00
S-Rad <shima.radfar@txs.de>
2020-10-28 15:57:06 +0100

    dialog background transparent

--- src/Door.js
+++ src/Door.js
@@ -1,8 +1,18 @@
 import { Box, Dialog } from '@material-ui/core'
 import { useState } from 'react'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    dialog: {
+        backgroundColor: 'transparent',
+    },
+})
 
 const Door = ({ day, imagepath }) => {
     const [open, setOpen] = useState(false)
+    const classes = useStyles()
+    console.log(useStyles())
+    console.log(classes)
     return (
         <>
             <Box height="100px" width="100px" display="flex" flexDirection="column" alignItems="center" onClick={e => setOpen(true)}>
@@ -10,7 +20,7 @@ const Door = ({ day, imagepath }) => {
                     {day}
                 </Box>
             </Box>
-            <Dialog open={open} onClose={e => setOpen(false)} onClick={e => setOpen(false)}>
+            <Dialog open={open} onClose={e => setOpen(false)} onClick={e => setOpen(false)} classes={{ paper: classes.dialog }}>
                 <img src={imagepath} alt="Ein kleines weihnachtliches Bildchen." />
             </Dialog>
         </>
