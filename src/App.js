07e26f3393d6aacfff1f620537e71f9682215742
Felix Tietjen <felix@tietjen.it>
2020-11-11 11:11:52 +0100

    Responsive Font Sizes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Door from './Door'
-import { Box } from '@material-ui/core'
+import { Box, createMuiTheme, responsiveFontSizes, ThemeProvider, Typography } from '@material-ui/core'
 import background from './assets/background-main.jpg'
 import Snowflakes from './Snowflakes'
 import CursorTrail from './CursorTrail'
@@ -12,31 +12,46 @@ const images = importAll(require.context('./assets/days', false, /\.(png|jpe?g|s
 
 const App = () => {
     return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            minHeight="100vh"
-            style={{
-                backgroundImage: `url(${background})`,
-                backgroundSize: 'cover',
-                fontFamily: "'Mountains of Christmas', cursive",
-                fontSize: '50px',
-            }}
+        <ThemeProvider
+            theme={responsiveFontSizes(
+                createMuiTheme({
+                    typography: {
+                        useNextVariants: true,
+                        h1: {
+                            color: 'white',
+                            fontFamily: "'Mountains of Christmas', cursive",
+                            textAlign: 'center',
+                        },
+                    },
+                })
+            )}
         >
-            <Snowflakes />
-            <CursorTrail />
-            <Box minHeight="100vh" display="flex" alignItems="center" zIndex="1">
-                <Box display="flex" flexDirection="column" alignItems="center">
-                    <h2 style={{ color: 'white', textAlign: 'center' }}>Willkommen beim TXS-Weihnachtszauber!</h2>
-                    <Box display="flex" width="80vw" flexWrap="wrap">
-                        {[8, 14, 23, 24, 3, 4, 12, 15, 2, 20, 19, 13, 21, 7, 11, 10, 16, 5, 9, 1, 17, 6, 22, 18].map(day => (
-                            <Door key={day} day={day} imagepath={images[day - 1]} />
-                        ))}
+            <Box
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                minHeight="100vh"
+                style={{
+                    backgroundImage: `url(${background})`,
+                    backgroundSize: 'cover',
+                    fontSize: '50px',
+                    color: 'white',
+                }}
+            >
+                <Snowflakes />
+                <CursorTrail />
+                <Box minHeight="100vh" display="flex" alignItems="center" zIndex="1">
+                    <Box display="flex" flexDirection="column" alignItems="center">
+                        <Typography variant="h1">Willkommen beim TXS-Weihnachtszauber!</Typography>
+                        <Box display="flex" width="80vw" flexWrap="wrap">
+                            {[8, 14, 23, 24, 3, 4, 12, 15, 2, 20, 19, 13, 21, 7, 11, 10, 16, 5, 9, 1, 17, 6, 22, 18].map(day => (
+                                <Door key={day} day={day} imagepath={images[day - 1]} />
+                            ))}
+                        </Box>
                     </Box>
                 </Box>
             </Box>
-        </Box>
+        </ThemeProvider>
     )
 }
 
